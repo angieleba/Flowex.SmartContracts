ecc0229690145d5a2a76748844655dad66a5a511
Tahlil <bsse0803@iit.du.ac.bd>
2023-02-13 00:25:31 +0600

    Add data structure to the contract

--- contracts/Flowex.sol
+++ contracts/Flowex.sol
@@ -5,4 +5,39 @@ contract Flowex {
     constructor() {
         
     }
+
+    enum WoodType {
+        SawLog,
+        PulpWood,
+        Biomass       
+    }
+
+    enum Shade{
+        Light,
+        Medium,
+        Dark
+    }
+
+    enum Unit {
+        CBM,
+        CFT
+    }
+
+    struct Product {
+        uint256 productID;
+        string treeType;
+        string location;
+        WoodType woodType;
+        Shade shade;
+        bool isRaw;
+        uint256 pricePerUnit;
+        string photo;
+        uint256 amount;
+        Unit unit;
+    }
+
+    mapping (string => Product[]) companyToProducts;
+
+
+
 }
