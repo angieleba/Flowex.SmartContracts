41a3cc352054a6b1eb6b0f3ac0c81d183588366a
Tahlil <bsse0803@iit.du.ac.bd>
2023-02-19 12:11:15 +0600

    Deploy usdc contract on testnet

--- scripts/createUSDCToken.js
+++ scripts/createUSDCToken.js
@@ -16,39 +16,43 @@ const operatorKey = PrivateKey.fromString(process.env.HEDERA_OPERATOR_PVKEY);
 
 const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
-//Create the transaction and freeze for manual signing
-const transaction = await new TokenCreateTransaction()
-    .setTokenName("USDC")
-    .setTokenSymbol("USDC")
-    .setTreasuryAccountId(operatorId)
-    .setInitialSupply(10000000000000)
-    .setDecimals(2)
-    .setAutoRenewAccountId(operatorId)
-    .setAutoRenewPeriod(11000000)
-    .setMaxTransactionFee(new Hbar(11))
-    .freezeWith(client);
+async function main() {
+    //Create the transaction and freeze for manual signing
+    const transaction = await new TokenCreateTransaction()
+        .setTokenName("USDC")
+        .setTokenSymbol("USDC")
+        .setTreasuryAccountId(operatorId)
+        .setInitialSupply(10000000000000)
+        .setDecimals(2)
+        .setAutoRenewAccountId(operatorId)
+        .setAutoRenewPeriod(5000000)
+        .setMaxTransactionFee(new Hbar(11))
+        .freezeWith(client);
 
-//Sign the transaction with the token treasury account private key
-const signTx = await transaction.sign(operatorKey);
+    //Sign the transaction with the token treasury account private key
+    const signTx = await transaction.sign(operatorKey);
 
-const txResponse = await signTx.execute(client);
+    const txResponse = await signTx.execute(client);
 
-//Get the receipt of the transaction
-const receipt = await txResponse.getReceipt(client);
+    //Get the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
 
-//Get the token ID from the receipt
-const tokenId = receipt.tokenId;
+    //Get the token ID from the receipt
+    const tokenId = receipt.tokenId;
 
-console.log("The new token ID is " + tokenId);
+    console.log("The new token ID is " + tokenId);
 
-const tokenAddress = "0x" + tokenId.toSolidityAddress();
+    const tokenAddress = "0x" + tokenId.toSolidityAddress();
 
-console.log(`USDC token smart contract ID in Solidity format is: ${tokenAddress} \n`);
-const contractBaseLink = "https://hashscan.io/testnet/contract/";
-fs.writeFileSync(
-    "TokenData.json",
-    JSON.stringify({
-        usdcAddress: tokenAddress,
-        usdcTokenContractLink: contractBaseLink + tokenId,
-    }, null, 4)
-);
+    console.log(`USDC token smart contract ID in Solidity format is: ${tokenAddress} \n`);
+    const contractBaseLink = "https://hashscan.io/testnet/contract/";
+    fs.writeFileSync(
+        "TokenData.json",
+        JSON.stringify({
+            usdcAddress: tokenAddress,
+            usdcTokenContractLink: contractBaseLink + tokenId,
+        }, null, 4)
+    );
+}
+
+main();
