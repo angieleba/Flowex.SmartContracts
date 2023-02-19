800dcf805bb845dae2f097789fbc8a058abed0c7
Tahlil <bsse0803@iit.du.ac.bd>
2023-02-19 20:00:42 +0600

    Add carbon footprint token

---
+++ scripts/createCarbonFootprintToken.js
@@ -0,0 +1,58 @@
+const path = require('path');
+var fs = require('fs');
+
+require("dotenv").config({ path: path.resolve(__dirname, '..', '.env') });
+const {
+    AccountId,
+    PrivateKey,
+    Client,
+    TokenCreateTransaction,
+    Hbar
+} = require("@hashgraph/sdk");
+
+// Configure accounts and client
+const operatorId = AccountId.fromString(process.env.HEDERA_OPERATOR_ID);
+const operatorKey = PrivateKey.fromString(process.env.HEDERA_OPERATOR_PVKEY);
+
+const client = Client.forTestnet().setOperator(operatorId, operatorKey);
+
+async function main() {
+    //Create the transaction and freeze for manual signing
+    const transaction = await new TokenCreateTransaction()
+        .setTokenName("Floex Carbon Footprint Token")
+        .setTokenSymbol("FXCFT")
+        .setTreasuryAccountId(operatorId)
+        .setInitialSupply(10000000000000)
+        .setDecimals(5)
+        .setAutoRenewAccountId(operatorId)
+        .setAutoRenewPeriod(6000000)
+        .setMaxTransactionFee(new Hbar(13))
+        .freezeWith(client);
+
+    //Sign the transaction with the token treasury account private key
+    const signTx = await transaction.sign(operatorKey);
+
+    const txResponse = await signTx.execute(client);
+
+    //Get the receipt of the transaction
+    const receipt = await txResponse.getReceipt(client);
+
+    //Get the token ID from the receipt
+    const tokenId = receipt.tokenId;
+
+    console.log("The new token ID is " + tokenId);
+
+    const tokenAddress = "0x" + tokenId.toSolidityAddress();
+
+    console.log(`Flowex carbon footprint token smart contract ID in Solidity format is: ${tokenAddress} \n`);
+    const contractBaseLink = "https://hashscan.io/testnet/contract/";
+    fs.writeFileSync(
+        "CFTokenData.json",
+        JSON.stringify({
+            CFTokenAddress: tokenAddress,
+            CFTokenContractLink: contractBaseLink + tokenId,
+        }, null, 4)
+    );
+}
+
+main();
